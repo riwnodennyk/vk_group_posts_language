19be4d5d98a7bb4703d4720ec8f033c6c78acbc7
Andrii Lavrinenko <forum_mail@ukr.net>
2015-03-29 20:09:54 +0300

    oziv and luhansk group ids added

--- LoadVkPosts.py
+++ LoadVkPosts.py
@@ -3,9 +3,11 @@ from enum import Enum
 import statistics
 import re
 import math
-
 import requests
 
+UA_REVO = -38854900
+LUHANSK = -2424065
+OZIV = -72444174
 
 __author__ = 'aindrias'
 
@@ -18,16 +20,9 @@ class Language(Enum):
     UNDEFINED = 3
 
 
-def load_html(offset):
+def load_html(group_id, offset):
     url = 'https://vk.com/al_wall.php'
-    data = 'act=get_wall&al=1&fixed=5509812&offset=' + offset.__str__() + '&owner_id=-38854900&type=own'
-    # headers = ['Origin: https://vk.com', 'X-Requested-With: XMLHttpRequest',
-    # 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36',
-    # 'Content-Type: application/x-www-form-urlencoded', 'Accept: */*', 'Referer: https://vk.com/uarevo',
-    # 'Accept-Encoding: gzip, deflate',
-    # 'Accept-Language: ro,tr;q=0.8,fr;q=0.6,de;q=0.4,be;q=0.2,uk;q=0.2,en-US;q=0.2,en;q=0.2,pl;q=0.2',
-    # 'Cookie: remixdt=-3600; remixstid=1997731857_d790546204bbbd6b2f; audio_time_left=0; remixtst=3f60ef96; remixlang=1; remixsid=e7d287cc16c88eaf1ecf035b484ad3106c82df4a5eba70da09999; remixsslsid=1; remixrefkey=23b3c7529ae25b9078; audio_vol=100; remixseenads=1; remixflash=17.0.0; remixscreen_depth=24',
-    # 'Cache-Control: no-cache', 'Postman-Token: 4954bd28-4b5c-bd92-7577-3749d7af9667']
+    data = 'act=get_wall&al=1&fixed=&offset=' + offset.__str__() + '&owner_id=' + group_id.__str__() + '&type=own'
     return requests.post(url, data).text
 
 
@@ -54,7 +49,7 @@ def parse_single_post(snippet):
     like = re.search('post_like_count fl_l" id="like_count-\d*_\d*">(\d*)</', snippet)
     share = re.search('post_share_count fl_l" id="share_count-\d*_\d*">(\d*)</', snippet)
     if message and like and share:
-        return Post(language(message.group(1)), int(like.group(1)), int(share.group(1)))
+        return Post(language(message.group(1)), int('0'+like.group(1)), int('0'+share.group(1)))
     else:
         # Either of message, likes count or shares count can't be parsed.
         # Usually it's message. Because posts happen be image-only or video-only.
@@ -72,14 +67,14 @@ def parse_posts(html):
 
 
 def print_list(numbers_list):
-    print('[%s]' % ', '.join(map(str, numbers_list)))
+    # print('[%s]' % ', '.join(map(str, numbers_list)))
     if numbers_list.__len__() == 0:
         pass
     else:
         print('MEAN '
               + (statistics.mean(numbers_list)).__int__().__str__()
               + ' MEDIAN '
-              + statistics.median(numbers_list).__int__().__str__())
+              + statistics.median(numbers_list).__int__().__str__(), end="")
         if numbers_list.__len__() > 1:
             print(' VARIANCE ' + math.sqrt(statistics.variance(numbers_list)).__int__().__str__())
 
@@ -123,11 +118,11 @@ def shares(posts):
     return [post.share for post in posts]
 
 
-def load_posts(count=20):
-    offset = 10
+def load_posts(group_id, count=50):
+    offset = 20
     posts = []
     while posts.__len__() < count:
-        posts.extend(parse_posts(load_html(offset)))
+        posts.extend(parse_posts(load_html(group_id, offset)))
         offset += 10
         print(posts.__len__(), '..')
     print('\n')
@@ -135,4 +130,4 @@ def load_posts(count=20):
 
 
 if __name__ == '__main__':
-    analyze(load_posts(50))
+    analyze(load_posts(OZIV, 1000))
